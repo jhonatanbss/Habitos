3f5a1d29bad84723ceca61a04147826b81b30db9
Jhonatanbss <jhonatanbruno7@hotmail.com>
2022-04-13 15:00:18 -0300

    Initial Commit

---
+++ Habitos/HabitosApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitosApp.swift
+//  Habitos
+//
+//  Created by Jhonatan Bruno Santos Silva on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
